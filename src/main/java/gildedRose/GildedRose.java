3ac4bf1d38310a5f661b2e8b8f65f09f3ac070ba
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-08-13 03:32:52 +0800

    add ItemValidator class for some function

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -2,6 +2,7 @@ package gildedRose;
 public class GildedRose {
     Item[] items;
     ItemUpdated itemUpdated=new ItemUpdated();
+    ItemValidator itemValidator=new ItemValidator();
     public GildedRose(Item[] items) {
         this.items = items;
     }
@@ -10,29 +11,29 @@ public class GildedRose {
         for (Item item:items) {
            switch(item.name){
                case "Aged Brie" :
-                   changeBackstage_passesQuality(item);
+                   itemValidator.changeBackstage_passesQuality(item);
                    item.sellIn--;
-                   if (isItemSellInMinus0(item)){
-                       IsMinus50forQuality(item);
+                   if (itemValidator.isItemSellInMinus0(item)){
+                       itemValidator.IsMinus50forQuality(item);
                    }
                    break;
                case "Backstage passes to a TAFKAL80ETC concert":
-                   changeBackstage_passesQuality(item);
+                   itemValidator.changeBackstage_passesQuality(item);
                    item.sellIn--;
-                   if (isItemSellInMinus0(item)){
+                   if (itemValidator.isItemSellInMinus0(item)){
                        item.quality = 0;
                    }
                    break;
                case "Sulfuras, Hand of Ragnaros":
-                   changeBackstage_passesQuality(item);
+                   itemValidator.changeBackstage_passesQuality(item);
                    break;
                default:
-                    if (notReachMinQuality(item)){
+                    if (itemValidator.notReachMinQuality(item)){
                        itemUpdated.decreaceQuality(item);
                     }
                     item.sellIn--;
-                    if (isItemSellInMinus0(item)){
-                        if (notReachMinQuality(item)){
+                    if (itemValidator.isItemSellInMinus0(item)){
+                        if (itemValidator.notReachMinQuality(item)){
                             itemUpdated.decreaceQuality(item);
                         }
                     }
@@ -42,42 +43,4 @@ public class GildedRose {
         }
     }
 
-    private boolean isItemSellInMinus0(Item item) {
-        return item.sellIn < 0;
-    }
-
-    private void IsMinus50forQuality(Item item) {
-        if (item.quality < 50) {
-            itemUpdated.addQuality(item);
-        }
-    }
-    private boolean notReachMinQuality(Item item) {
-        return item.quality > 0;
-    }
-
-    public void changeBackstage_passesQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality ++;
-            if (isEqualsBackstage_passes(item)) {
-                if (item.sellIn < 11) {
-                    IsMinus50forQuality(item);
-                }
-
-                if (item.sellIn < 6) {
-                    IsMinus50forQuality(item);
-                }
-            }
-        }
-    }
-    private boolean isEqualsSulfuras(Item item) {
-        return item.name.equals("Sulfuras, Hand of Ragnaros");
-    }
-
-    private boolean isEqualsBackstage_passes(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    private boolean isAgedBrie(Item items){
-        return  items.name.equals("Aged Brie");
-    }
 }
---
+++ src/main/java/gildedRose/ItemValidator.java
@@ -0,0 +1,43 @@
+package gildedRose;
+
+public class ItemValidator {
+    ItemUpdated itemUpdated=new ItemUpdated();
+    public boolean isItemSellInMinus0(Item item) {
+        return item.sellIn < 0;
+    }
+
+    public void IsMinus50forQuality(Item item) {
+        if (item.quality < 50) {
+            itemUpdated.addQuality(item);
+        }
+    }
+    protected boolean notReachMinQuality(Item item) {
+        return item.quality > 0;
+    }
+
+    public void changeBackstage_passesQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality ++;
+            if (isEqualsBackstage_passes(item)) {
+                if (item.sellIn < 11) {
+                    IsMinus50forQuality(item);
+                }
+
+                if (item.sellIn < 6) {
+                    IsMinus50forQuality(item);
+                }
+            }
+        }
+    }
+    protected boolean isEqualsSulfuras(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    protected boolean isEqualsBackstage_passes(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    protected boolean isAgedBrie(Item items){
+        return  items.name.equals("Aged Brie");
+    }
+}
