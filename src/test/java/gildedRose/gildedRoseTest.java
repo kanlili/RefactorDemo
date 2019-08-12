e82d91d6e6f5639ddfefc91dfda8e5d57884a21b
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-08-12 20:44:34 +0800

    add test: items_quality is 3 ,name is Backstage passes and sillin minus 1

--- src/test/java/gildedRose/gildedRoseTest.java
+++ src/test/java/gildedRose/gildedRoseTest.java
@@ -8,7 +8,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class gildedRoseTest {
-
+    private  static final String Other_Name="Other Name";
+    private  static final String Aged_Brie="Aged Brie";
+    private  static final String Backstage_passes="Backstage passes to a TAFKAL80ETC concert";
+    private  static final String Sulfuras_Hand="Sulfuras, Hand of Ragnaros";
     @Test
     public void should_return_2_when_call_updateQuality_given_items_qualty3(){
         Item item=new Item("Backstage passes",10,3);
@@ -17,5 +20,12 @@ public class gildedRoseTest {
         gildedRose.updateQuality();
         Assert.assertEquals(2,item.quality);
     }
-
+    @Test
+    public void should_return_1_when_call_updateQuality_given_items_qualty3_sillin_minus1(){
+        Item item=new Item("Backstage passes",-1,3);
+        Item[] items=new Item[]{item};
+        GildedRose gildedRose=new GildedRose(items);
+        gildedRose.updateQuality();
+        Assert.assertEquals(1,item.quality);
+    }
 }
