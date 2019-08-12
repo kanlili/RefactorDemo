f1588c86ae8780ce31eb1910ec3ddbb3fbf913a1
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-08-13 00:02:24 +0800

    small refactor for updateQuality function

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -8,43 +8,43 @@ public class GildedRose {
 
     public void updateQuality() {
         for (Item item:items) {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!isAgedBrie(item)
+                    && !isEqualsBackstage_passes(item)) {
                 if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
+                    if (!isEqualsSulfuras(item)) {
+                        decreaceQuality(item);
                     }
                 }
             } else {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    addQuality(item);
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (isEqualsBackstage_passes(item)) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1;
+                                addQuality(item);
                             }
                         }
 
                         if (item.sellIn < 6) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1;
+                                addQuality(item);
                             }
                         }
                     }
                 }
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!isEqualsSulfuras(item)) {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!isAgedBrie(item)) {
+                    if (!isEqualsBackstage_passes(item)) {
                         if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
+                            if (!isEqualsSulfuras(item)) {
+                                decreaceQuality(item);
                             }
                         }
                     } else {
@@ -52,11 +52,30 @@ public class GildedRose {
                     }
                 } else {
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                        addQuality(item);
                     }
                 }
             }
         }
     }
 
+    private void addQuality(Item item) {
+        item.quality = item.quality + 1;
+    }
+
+    private void decreaceQuality(Item item) {
+        item.quality = item.quality - 1;
+    }
+
+    private boolean isEqualsSulfuras(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    private boolean isEqualsBackstage_passes(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private boolean isAgedBrie(Item items){
+        return  items.name.equals("Aged Brie");
+    }
 }
